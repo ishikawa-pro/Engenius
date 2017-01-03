3c4b6c39aec536712eef4923346cbe20f57c0123
ishikawa-pro <ishikawa.pro@gmail.com>
2017-01-03 13:47:53 +0900

    db/schema.rb とarticlesとcategoriesのfixturesを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161230103124) do
+ActiveRecord::Schema.define(version: 20170101145916) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,9 +20,9 @@ ActiveRecord::Schema.define(version: 20161230103124) do
     t.text     "link"
     t.date     "post_date"
     t.integer  "category_id"
-    t.binary   "image"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "image_url"
     t.index ["category_id"], name: "index_articles_on_category_id", using: :btree
   end
 
