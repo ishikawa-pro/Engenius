cd375381c82ea7ac27f1d1908c85b25d66830014
ishikawa-pro <ishikawa.pro@gmail.com>
2017-01-04 01:42:00 +0900

    index.jsonにcategoryを追加

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -2,7 +2,7 @@ class ArticleController < ApplicationController
     def index
         #render text: 'index'
         created_data = Article.maximum('created_at')
-        @articles = Article.where(created_at: created_data).select(:title, :link, :post_date, :image_url)
+        @articles = Article.where(created_at: created_data).select(:title, :link, :post_date, :image_url, :category_id)
     end
 
     def show
