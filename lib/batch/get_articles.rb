0be9db837628b2026326282a8e527e40babf7356
ishikawa-pro <ishikawa.pro@gmail.com>
2017-01-14 00:59:26 +0900

    スクレイピング用バッチファイルの完成

--- lib/batch/get_articles.rb
+++ lib/batch/get_articles.rb
@@ -2,15 +2,33 @@ require 'nokogiri'
 #require 'hatena_bookmark_provider.rb'
 
 class Batch::GetArticles
+    #既に登録済みの記事かどうか
+    def self.has_article?(link)
+        Article.where(link: link).count == 0
+    end
+
     def self.get_articles
-        url = 'http://b.hatena.ne.jp/search/title?safe=on&q=docker&users=50'
-        hb_provider = Hb_provide_lib::Hatena_bookmark_provider.new(url)
-        p Category.all
-        while hb_provider.has_next?
-            article = hb_provider.next_item
-            #記事以外のデータが混じるため、titleの有無でフィルタリング
-            if article['title'] != nil
-                puts article['title']
+        Category.select("category, source_url, id").each do|category|
+            hb_provider = Hb_provide_lib::Hatena_bookmark_provider.new(category.source_url)
+            while hb_provider.has_next?
+                article = hb_provider.next_item
+                #記事以外のデータが混じるため、titleの有無でフィルタリング
+                if article['title'] != nil
+                    article['category_id'] = category.id
+                    if has_article?(article['link']) 
+                        puts "未登録記事です"                        
+                        puts "title #{article['title']}"
+                        puts "url #{article['link']}"
+                        new_article = Article.new(article)
+                        if new_article.save
+                            puts "成功"
+                        end
+                    else
+                        puts "登録済みです"
+                        puts "title #{article['title']}"
+                        puts "url #{article['link']}"
+                    end
+                end
             end
         end
     end
