28b8b7a5d3b26b998bd244537df470436e5a1aeb
ishikawa-pro <ishikawa.pro@gmail.com>
2017-01-14 01:16:36 +0900

    lib/batch/get_articles.rbへコメントの追加

--- lib/batch/get_articles.rb
+++ lib/batch/get_articles.rb
@@ -8,20 +8,26 @@ class Batch::GetArticles
     end
 
     def self.get_articles
+        #カテゴリごとにイテレートする
         Category.select("category, source_url, id").each do|category|
+            #各記事を取得
             hb_provider = Hb_provide_lib::Hatena_bookmark_provider.new(category.source_url)
             while hb_provider.has_next?
                 article = hb_provider.next_item
                 #記事以外のデータが混じるため、titleの有無でフィルタリング
                 if article['title'] != nil
                     article['category_id'] = category.id
+                    #未登録の記事かどうか
                     if has_article?(article['link']) 
                         puts "未登録記事です"                        
                         puts "title #{article['title']}"
                         puts "url #{article['link']}"
+                        #データベースに保存
                         new_article = Article.new(article)
                         if new_article.save
                             puts "成功"
+                        else
+                            puts "失敗"
                         end
                     else
                         puts "登録済みです"
