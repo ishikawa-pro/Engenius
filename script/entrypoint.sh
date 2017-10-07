41a34d9002ad7e087173445e7ae944e3a2253e27
ishikawa-pro <ishikawa.pro@gmail.com>
2017-10-07 14:44:55 +0900

    server.pidがあるかないかで初期化処理をするかを決める
    一度serverを立てるとtmp/pids/server.pidができて、それが残った状態でコンテナを再起動すると、webrickが起動しないのでserver.pid有無を確認して初期化処理を行ったりする。

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
-rake db:create
-rake db:migrate
-rake db:seed
-bundle exec rails runner Batch::GetArticles.get_articles &
-rails s -b 0.0.0.0
+if [ ! -e tmp/pids/server.pid ]; then
+    sleep 3
+    rake db:create
+    rake db:migrate
+    rake db:seed
+else
+    #コンテナを落とすと、server.pidが残って次回正常にserverが立たないため
+    rm tmp/pids/server.pid
+fi
+rails s -d -b 0.0.0.0
+
+bundle exec rails runner Batch::GetArticles.get_articles
