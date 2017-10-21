c0405226269c44c61cd672bab0c449822446f17b
ishikawa-pro <ishikawa.pro@gmail.com>
2017-10-21 22:33:52 +0900

    springを使うから、bin/rake でコマンドを実行しないといけない
    rails4.1からSpringが標準で組み込まれている関係で、bundle exec rake
    ではなくbin/rakeで実行しないとspringが起動しない

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -4,9 +4,9 @@ if [ ! -e tmp/pids/server.pid ]; then
     #postgresのセットアップ終わるまでに、立ち上がることがあるので
     #暫定措置として3秒待つ
     sleep 3
-    rake db:create
-    rake db:migrate
-    rake db:seed
+    bin/rake db:create
+    bin/rake db:migrate
+    bin/rake db:seed
 else
     #コンテナを落とすと、server.pidが残って次回正常にserverが立たないため
     rm tmp/pids/server.pid
