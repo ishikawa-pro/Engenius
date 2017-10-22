dbfa3dc226947e5628ef3ce623bf379bbb22f228
ishikawa-pro <ishikawa.pro@gmail.com>
2017-10-22 12:28:51 +0900

    wheneverは使わないので除外と、最初にdbの削除コマンドを追加

--- engenius/script/entrypoint.sh
+++ engenius/script/entrypoint.sh
@@ -4,10 +4,10 @@ if [ ! -e tmp/pids/server.pid ]; then
     #postgresのセットアップ終わるまでに、立ち上がることがあるので
     #暫定措置として3秒待つ
     sleep 3
+    bin/rake db:drop
     bin/rake db:create
     bin/rake db:migrate
     bin/rake db:seed
-    bundle exec wheneverize . 
 else
     #コンテナを落とすと、server.pidが残って次回正常にserverが立たないため
     rm tmp/pids/server.pid
