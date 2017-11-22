247055b17deaafd5b47b20bdab7e0f1977bf90d6
ishikawa-pro <ishikawa.pro@gmail.com>
2017-11-23 00:42:31 +0900

    指定したカテゴリの最新記事を日付順に取ってくるように修正

--- engenius/app/controllers/article_controller.rb
+++ engenius/app/controllers/article_controller.rb
@@ -1,10 +1,12 @@
 class ArticleController < ApplicationController
     def index
-        #render text: 'index'        
-        @articles = Article.select(:title, :link, :post_date,
-                                   :image_url, :category_id)
-                            .order(post_date: :desc)
-                            .offset(params['offset']).limit(params['limit'])
+        p params['category']
+        @articles = Category.joins(:articles)
+                    .select("categories.category, articles.*")
+                    .where(category: params['category'])
+                    .order("articles.post_date desc")
+                    .offset(params['offset'])
+                    .limit(params['limit'])
     end
 
     def show
