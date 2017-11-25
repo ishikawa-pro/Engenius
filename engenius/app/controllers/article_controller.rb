44f674423f83df4658e11dc50c608c8479bb4318
ishikawa-pro <ishikawa.pro@gmail.com>
2017-11-25 10:20:39 +0900

    パラメータクエリで複数のカテゴリを”+”で繋げて指定できるように修正
    ex) /article.json?category=Swift+Docker

--- engenius/app/controllers/article_controller.rb
+++ engenius/app/controllers/article_controller.rb
@@ -1,9 +1,10 @@
 class ArticleController < ApplicationController
     def index
         p params['category']
+        categories = params['category'].split(" ")
         @articles = Category.joins(:articles)
                     .select("categories.category, articles.*")
-                    .where(category: params['category'])
+                    .where(category: categories)
                     .order("articles.post_date desc, id")
                     .offset(params['offset'])
                     .limit(params['limit'])
