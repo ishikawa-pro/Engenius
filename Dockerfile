ada059624142c8ee3210b303673c9c81a859e173
石川諒 <ishikawa.pro@gmail.com>
2018-03-03 20:36:49 +0900

    heroku container registory用にDockerfileやRailsの設定を修正

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,10 @@
 FROM rails:latest
 MAINTAINER akira
 
+ENV RAILS_ENV=production RACK_ENV=production
 WORKDIR /usr/src/engenius
 COPY ./engenius/Gemfile* ./
 RUN bundle install
 COPY ./engenius ./
 
-ENTRYPOINT ["./script/entrypoint.sh"]
+CMD ["./script/entrypoint.sh"]
--- engenius/script/entrypoint.sh
+++ engenius/script/entrypoint.sh
@@ -1,17 +1,4 @@
 #!/bin/sh
 
-if [ ! -e tmp/pids/server.pid ]; then
-    #postgresのセットアップ終わるまでに、立ち上がることがあるので
-    #暫定措置として3秒待つ
-    sleep 3
-    bin/rake db:drop
-    bin/rake db:create
-    bin/rake db:migrate
-    bin/rake db:seed
-else
-    #コンテナを落とすと、server.pidが残って次回正常にserverが立たないため
-    rm tmp/pids/server.pid
-fi
-
-bundle exec rails runner Batch::GetArticles.get_articles &
+#bin/rails runner Batch::GetArticles.get_articles &
 rails s -b 0.0.0.0
