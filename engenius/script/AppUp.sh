7f5cbb4dde6712e4215a74f18e29996b7dd75d9d
石川諒 <ishikawa.pro@gmail.com>
2018-03-03 21:02:21 +0900

    不要なスクリプトファイルを削除

--- engenius/script/AppUp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ -e tmp/pids/server.pid ]; then
-    #コンテナを落とすと、server.pidが残って次回正常にserverが立たないため
-    rm tmp/pids/server.pid
-fi
-
-bundle exec rails runner Batch::GetArticles.get_articles &
-rails s -b 0.0.0.0
--- engenius/script/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-#bin/rails runner Batch::GetArticles.get_articles &
-rails s -b 0.0.0.0
